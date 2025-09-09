6da72440b3d186bf113cba2fbb7c5f55481ff5b7
mirazorovbadavlat-netizen <mirazorovbadavlat@gmail.com>
2025-09-09 17:05:31 +0300

    Add files via upload

---
+++ index.html.js
@@ -0,0 +1,40 @@
+
+var text = document.getElementById(`text`);
+console.log(text.id);
+text.style.color = "red";
+let t = document.getElementById(`f`)
+console.log(f.id) 
+t.style.backgroundColor = "bisque"
+
+// function createElement(h1, b, id) {
+// let element = document.createElement(h1)
+// if (id) {
+//   element.id = id
+// }
+// element.innerh1 = h1
+// return  element
+// }
+
+
+// function LogArgs () {
+//   console.log(arguments)
+//   for (let argument in arguments)
+//     console.log(argument)
+//   console.log (`Sosal?`)
+// }
+// LogArgs(1, 2, 5, 7,)
+// let user = {
+//     firstname: `Lia`,
+//     sayhi() {
+// let arrow = () => alert (this.firstname)
+// arrow()
+//     }
+// } 
+// user.sayhi()
+calculator = {
+
+}
+
+let c = +prompt(`Введите число`, )
+let a = c
+let b = c 
